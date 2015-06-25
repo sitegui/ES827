dde25cf508b7670e8bf40446bdafbc2b3b2ac12a
Rooneysan <augustomgarcia@gmail.com>
2015-06-24 22:41:57 -0300

    Adicionado gif por html

--- projeto2.m
+++ projeto2.m
@@ -4,10 +4,11 @@ close all;
 robot = p560.nofriction();
 
 n = 25;
-r = 0.5;
+r1 = 0.4;
+r2 = 0.75;
 z = -0.4;
 angle = linspace(0, 2*pi, n)';
-points = [r*cos(angle), r*sin(angle), z*ones(n, 1)];
+points = [r1*cos(angle), r2*sin(angle), z*ones(n, 1)];
 T = transl(points);
 q = robot.ikine(T);
 
@@ -19,10 +20,18 @@ frame = getframe(gcf);
 [im, map] = rgb2ind(frame.cdata, 256, 'nodither');
 for k = 1:size(q,1)
 	robot.plot(q(k, :));
-	plot2(points(k, :), 'r.');
+ 	plot2(points(k, :), 'r.');
 	frame = getframe(gcf);
 	im(:,:,1,k) = rgb2ind(frame.cdata, map, 'nodither');
 end
-imwrite(im, map, 'gui.gif', 'DelayTime', 0, 'LoopCount', inf);
+imwrite(im, map, 'html\gui.gif', 'DelayTime', 0, 'LoopCount', inf);
+
+
+
+%% Resulting robot movement
+% <html>
+% <img src="gui.gif" alt="Elipse no chão para o robô Puma560." align = "middle" > 
+% </html>
+%
 
 end
