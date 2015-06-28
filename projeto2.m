45703cac8ab4d55b8440872eae86a73706239011
Guilherme Souza <sitegui@sitegui.com.br>
2015-06-28 15:10:51 -0300

    Coloquei as duas elipses na mesma animação

--- projeto2.m
+++ projeto2.m
@@ -2,52 +2,63 @@ function projeto2(p560)
 close all;
 robot = p560.nofriction();
 
-%% 2.2.a Desenho de elipse no chão do espaço de trabalho com o robô puma560
+%% 2.2 Desenho de elipse no chão e na parede do espaço de trabalho com o robô puma560
 
-n = 25;
-r1 = 0.4;
-r2 = 0.75;
-z = -0.35;
-angle = linspace(0, 2*pi, n)';
-points = [r1*cos(angle), r2*sin(angle), z*ones(n, 1)];
-T = transl(points);
+% Primeira elipse
+n1 = 50;
+rx = 0.5;
+ry = 0.7;
+z = -0.5;
+angle = linspace(0, 2*pi, n1)';
+points1 = [rx*cos(angle), ry*sin(angle), z*ones(n1, 1)];
+T1 = transl(points1);
+
+% Segunda elipse
+n2 = 50;
+x = 0.5;
+ry = 0.5;
+rz = 0.25;
+angle = linspace(0, 2*pi, n2)';
+points2 = [x*ones(n2, 1), ry*sin(angle), rz*cos(angle)];
+T2 = transl(points2);
+
+% Caminho entre as elipses
+n12 = 20;
+T12 = ctraj(T1(:, :, end), T2(:, :, 1), n12);
+T = cat(3, T1, T12, T2);
+
+% Rotação final
+n3 = 25;
+az = linspace(-37.5, -37.5+360, n3);
+
 q = robot.ikine(T);
-
-% figure;
-% set(gcf, 'Renderer', 'zbuffer');
-% robot.plot(q(1,:));
-% hold('on');
-% frame = getframe(gcf);
-%[im, map] = rgb2ind(frame.cdata, 256, 'nodither');
 
 	function plotFrame(n)
-		robot.plot(q(n, :));
-		hold('on');
-		plot2(points(n, :), 'r.');
+		if n <= n1+n12+n2
+			robot.plot(q(n, :), 'workspace', [-0.5, 0.5, -0.75, 0.75, -0.5, 0.75]);
+		end
+		
+		if n <= n1
+			hold('on');
+			plot2(points1(n, :), 'r.');
+		elseif n <= n1+n12
+		elseif n <= n1+n12+n2
+			hold('on');
+			plot2(points2(n-n1-n12, :), 'r.');
+		else
+			axis('off');
+			view(az(n-n1-n12-n2), 30);
+		end
 	end
 
-plot2gif(size(q, 1), @plotFrame, 'html\2.1.gif');
-%%
-% <html>
-% <p align="center"><img src="2.1.gif" alt="Elipse no chão para o robô Puma560."></p> 
-% </html>
-%
+plot2gif(n1+n12+n2+n3, @plotFrame, 'html/elipse.gif');
+close('all');
 
-%% 2.1.b Desenho de elipse na parede do espaço de trabalho com o robô puma560
-n = 25;
-r1 = 0.2;
-r2 = 0.4;
-x = 0.6;
-angle = linspace(0, 2*pi, n)';
-points = [x*ones(n, 1), r2*sin(angle), r1*cos(angle)];
-T = transl(points);
-q = robot.ikine(T);
-
-plot2gif(size(q, 1), @plotFrame, 'html\2.2.gif');
 %%
+% 
 % <html>
-% <p align="center"><img src="2.2.gif" alt="Elipse na parede para o robô Puma560."></p> 
+% <p align="center"><img src="elipse.gif" alt="Elipse no chão e na parede para o robô Puma560."></p>
 % </html>
-%
+% 
 
 end
